3c8c0ecb73c142f70c14dbb3511627188c1daf6c
Twitch1209 <39025598+Twitch1209@users.noreply.github.com>
2018-12-10 22:16:11 +0800

    Problem6 Finished

--- Problem6/Problem6.cpp
+++ Problem6/Problem6.cpp
@@ -24,38 +24,45 @@ int main()
 	cout << "请输入祖先的姓名：";
 	cin >> ancName;
 	cout << "此家庭的祖先是：" << ancName;
-	cout << endl << endl;;
+	cout << endl;
 	t->BuildRoot(ancName);
 	vector<string> chName;
 	while (1)
 	{
+		cout << endl;
 		cout << "请输入要执行的操作：";
 		cin >> order;
-		cout << endl;
 		switch (order)
 		{
 		    case 'A':
 			{
+				//t是二叉树，childNum是子女数量，parName为建立家庭的人,chName记录了要加入家庭的子女名
 				cout << "请输入要建立家庭的人的姓名：";
 				string parName;
 				cin >> parName;
+				//没有发现这个人
+				if (!t->Find(parName))
+				{
+					cout << "查无此人" << endl;
+					continue;
+				}
 				cout << "请输入" << parName << "的儿女人数：";
 				int childNum;
 				cin >> childNum;
 				cout << "请依次输入" << parName << "的儿女的姓名：";
+				chName.clear();
+				//输入要加入家庭的子女们
 				for (int i = 0; i < childNum; i++)
 				{
 					string childName;
 					cin >> childName;
 					chName.push_back(childName);
 				}
+				//子女们加入家庭，都成为第一个子节点的兄弟
 				t->AddChild(parName, chName[0]);
-				for (int i = 0; i < childNum; i++)
+				for (int i = 0; i < childNum-1; i++)
 				{
-					if (i != childNum - 1)
-					{
-						t->AddSibling(chName[i], chName[i + 1]);
-					}
+					t->AddSibling(chName[i], chName[i + 1]);
 				}
 				cout << parName << "的第一代子孙是：";
 				t->showAllChild(parName);
@@ -64,9 +71,17 @@ int main()
 			}
 			case 'B':
 			{
+				//t是二叉树，parName为建立家庭的人，childName是新添加的子女的姓名
+				//如果父节点原来没有子女，直接添加，否则加在最后
 				cout << "请输入要添加儿子（或女儿）的人的姓名：";
 				string parName;
 				cin >> parName;
+				//没有发现这个人
+				if (!t->Find(parName))
+				{
+					cout << "查无此人" << endl;
+					continue;
+				}
 				cout << "请输入" << parName << "新添加的儿子（或女儿）的姓名：";
 				string childName;
 				cin >> childName;
@@ -79,7 +94,9 @@ int main()
 					string finalSibling = t->GetFinalSibling(parName);
 					t->AddSibling(finalSibling, childName);
 				}
-				cout << t->GetChild(parName);
+				cout << parName << "的第一代子孙是：";
+				t->showAllChild(parName);
+				cout << endl;
 				continue;
 			}
 			case 'C':
@@ -87,13 +104,17 @@ int main()
 				cout << "请输入要解散家庭的人的姓名：";
 				string parName;
 				cin >> parName;
+				//没有发现这个人
 				if (!t->Find(parName))
 				{
 					cout << "查无此人" << endl;
+					continue;
 				}
-				cout << "要解散家庭的人是：" << parName;
+				cout << "要解散家庭的人是：" << parName <<endl;
+				cout << parName << "的第一代子孙是："; 
+				t->showAllChild(parName);
 				t->RemoveFromPar(parName);
-				cout << t->GetChild(parName);
+				cout << endl;
 				continue;
 			}
 			case 'D':
@@ -101,7 +122,13 @@ int main()
 				cout << "请输入要更改姓名的人的目前姓名：";
 				string nowName;
 				cin >> nowName;
-				cout << "请输入更改后的姓名";
+				//没有发现这个人
+				if (!t->Find(nowName))
+				{
+					cout << "查无此人" << endl;
+					continue;
+				}
+				cout << "请输入更改后的姓名:";
 				string chName;
 				cin >> chName;
 				t->ChangeName(nowName, chName);
@@ -109,11 +136,23 @@ int main()
 				{
 					cout << nowName << "已经更名为" << chName;
 				}
+				else
+				{
+					cout << "查无此人" << endl;
+					continue;
+				}
+				cout << endl;
 				continue;
 			}
 			case 'E':
 			{
 				exit(1);
+			}
+			//如果用户输入操作要求错误，提示用户重新输入
+			default:
+			{
+				cout << "抱歉，您想要进行的操作不存在，请重新输入！" << endl;
+				continue;
 			}
 		}
 
--- Problem6/Tree.h
+++ Problem6/Tree.h
@@ -14,41 +14,25 @@ template<class T>
 class Tree {
 public:
 	Tree() { root = current = NULL; }
-	bool Root();									//让树的根节点成为树的当前节点
 	bool IsEmpty() { return root == NULL; }			
-	bool FirstChild();
-	bool NextSibling();
+	bool FirstChild();								//在树中找到当前节点的长子，并使之成为当前节点
+	bool NextSibling();								//在树中找到当前节点的下一个兄弟，并使之成为当前节点
 	bool Parent();									//在树中找当前节点的父节点，使之成为当前节点
-	bool Find(T target);
-	void AddChild(T parName, T childName);
-	void AddSibling(T childName, T Sibling);
-	void BuildRoot(T rootVal);
-	void showAllChild(T parName);
-	bool RemoveFromPar(T parName);
-	T GetFinalSibling(T parName);
-	T GetChild(T parName);
-	void ChangeName(T nowName, T chName);
+	bool Find(T target);							//在树中搜索含有target的结点，使之成为当前结点
+	void AddChild(T parName, T childName);			//添加左子女结点
+	void AddSibling(T childName, T Sibling);		//添加右兄弟结点
+	void BuildRoot(T rootVal);						//建立树的根结点,并使之成为树的当前结点
+	void showAllChild(T parName);					//展示t结点的所有孩子
+	bool RemoveFromPar(T parName);					//删除t结点
+	T GetFinalSibling(T parName);					//得到当前结点的最后一个兄弟
+	void ChangeName(T nowName, T chName);			//将nowName修改成chName
 private:
-	TreeNode<T> *root, *current;
-	bool Find(TreeNode<T>*p, T value);
-	void RemovesubTree(TreeNode<T> *p);
-	bool FindParent(TreeNode<T>*t, TreeNode < T >*p);
+	TreeNode<T> *root, *current;//指针指向根结点和当前结点
+	bool Find(TreeNode<T>*p, T value);//在以p为根的树中搜索value
+	void RemovesubTree(TreeNode<T> *p);//删除以p为根的子树
+	bool FindParent(TreeNode<T>*t, TreeNode < T >*p);//在以t为根节点的树中搜索p的父节点
 };
 
-template<class T>
-bool Tree<T>::Root()
-{
-	if (root == NULL)
-	{
-		current = NULL;
-		return false;
-	}
-	else
-	{
-		current = root;
-		return true;
-	}
-}
 
 template<class T>
 void Tree<T>::showAllChild(T parName)
@@ -71,13 +55,15 @@ void Tree<T>::showAllChild(T parName)
 
 template <class T> 
 void Tree <T> ::BuildRoot(T rootVal) 
-{ //建立树的根结点,并使之成为树的当前结点 
+{ 
+	//建立树的根结点,并使之成为树的当前结点 
 	root= current= new TreeNode<T> (rootVal);
 }
 
 template<class T>
 void Tree<T>::AddChild(T parName, T childName)
 {
+	//将当前结点设为parName，将它的子节点设为childName
 	if (Find(parName))
 	{
 		TreeNode<T> *p = new TreeNode<T>(childName);
@@ -88,6 +74,7 @@ void Tree<T>::AddChild(T parName, T childName)
 template<class T>
 void Tree<T>::AddSibling(T childName, T Sibling)
 {
+	//将当前结点设为childName，将它的右兄弟设为Sibling
 	if (Find(childName))
 	{
 		TreeNode<T> *s = new TreeNode<T>(Sibling);
@@ -168,20 +155,14 @@ T Tree<T>::GetFinalSibling(T parName)
 		}
 		return current->data;
 	}
+	return NULL;
 }
 
-template<class T>
-T Tree<T>::GetChild(T parName)
-{
-	if (Find(parName))
-	{
-		return current->firstChild->data;
-	}
-}
 
 template<class T>
 bool Tree<T>::RemoveFromPar(T parName)
 {
+	//找到当前结点并且删除
 	if (Find(parName))
 	{
 		current->firstChild = NULL;
@@ -190,6 +171,7 @@ bool Tree<T>::RemoveFromPar(T parName)
 	}
 	return false;
 }
+
 template<class T>
 bool Tree<T>::Find(T value)
 {
@@ -227,6 +209,7 @@ void Tree<T>::RemovesubTree(TreeNode<T> *p)
 template<class T>
 void Tree<T>::ChangeName(T nowName, T chName)
 {
+	//如果找到了nowName就更改其名字
 	if (Find(nowName))
 	{
 		current->data = chName;
